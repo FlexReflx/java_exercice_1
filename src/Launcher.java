a4a9cf7ab64d853619a372453f3a3dc3e1af6d22
Leo Bendref <leo.bendref@gmail.com>
2021-11-29 15:10:57 +0100

    première commande

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,8 +3,25 @@ public class Launcher {
     public static void main(String[] args) {
         System.out.println("Bienvenue dans l'antre du démon");
         Scanner sc = new Scanner(System.in);
-        while(sc.next().compareTo("quit") != 0){
-            System.out.println("Unknown command");
+        String i = sc.next();
+        sc.nextLine();
+        while(i.compareTo("quit") != 0){
+            switch(i){
+                case "fibo":
+                    System.out.println("suite de fibonacci \n");
+                    int fibo = sc.nextInt();
+                    sc.nextLine();
+                    int res =0;
+                        for (int j=0;j<=fibo;j++){
+                            res += j;
+                        }
+                    System.out.println("la suite de fibo vaut : "+ res+"\n");
+                    break;
+                default:
+                    System.out.println("Unknown command + \n");
+            }
+            i =sc.next();
+            sc.nextLine();
         }
         System.out.println("sortie standard");
         /*if(sc.next().compareTo("quit") == 0 ){
