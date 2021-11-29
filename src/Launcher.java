d890d9b2c9666d71bbe59498a7ad454b9483d9b6
Leo Bendref <leo.bendref@gmail.com>
2021-11-29 14:51:49 +0100

    Launcher v2

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,10 +3,14 @@ public class Launcher {
     public static void main(String[] args) {
         System.out.println("Bienvenue dans l'antre du démon");
         Scanner sc = new Scanner(System.in);
-        if(sc.next().compareTo("quit") == 0 ){
+        while(sc.next().compareTo("quit") != 0){
+            System.out.println("Unknown command");
+        }
+        System.out.println("sortie standard");
+        /*if(sc.next().compareTo("quit") == 0 ){
             System.out.println("sortie de programme");
         }else{
             System.out.println("unknown command");
-        }
+        }*/
     }
 }
